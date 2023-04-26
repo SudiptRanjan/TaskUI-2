241c068f934cec19fd885c28099cdc98faf3d03d
SudiptRanjan <130958812+SudiptRanjan@users.noreply.github.com>
2023-04-26 13:33:22 +0530

    Have done the all the screens and navigation part is half done
    Have done the all the screens and navigation part is half done

---
+++ Assets/MenuButton.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public Canvas bankCanvas;
+    public Canvas basicCanvas;
+    public Canvas statisticsCanvas;
+    public Canvas kycCanvas;
+    public Canvas homeCanvas;
+    public Canvas sideCanvas;
+    public Canvas tableListCanvas;
+    public Canvas popUpCanvas;
+    //private void Start()
+    //{
+    //    //bankButton = gameObject.GetComponent<Canvas>();
+    //    //basicButton = gameObject.GetComponent<Canvas>();
+    //    //statisticsButton = gameObject.GetComponent<Canvas>();
+    //    //kycButton = gameObject.GetComponent<Canvas>();
+    //}
+
+
+    //public void Update()
+    //{
+
+    //}
+    public void Profile_Onclick()
+    {
+        BankButton_OnClick();
+        BasicButton_OnClick();
+        StatisticsButton_OnClick();
+        KYCButton_OnClick();
+    }
+    public void BankButton_OnClick()
+    {
+        
+        if(bankCanvas.gameObject.activeInHierarchy==true)
+        {
+            bankCanvas.gameObject.SetActive(false);
+            bankCanvas.enabled = false;
+        }
+        else
+        {
+            bankCanvas.gameObject.SetActive(true);
+            bankCanvas.enabled = true;
+        }
+       
+    }
+    public void BasicButton_OnClick()
+    {
+        if (basicCanvas.gameObject.activeInHierarchy == true)
+        {
+            basicCanvas.gameObject.SetActive(false);
+            basicCanvas.enabled = false;
+        }
+        else
+        {
+            basicCanvas.gameObject.SetActive(true);
+            basicCanvas.enabled = true;
+        }
+
+
+    }
+
+    public void StatisticsButton_OnClick()
+    {
+        if (statisticsCanvas.gameObject.activeInHierarchy == true)
+        {
+            statisticsCanvas.gameObject.SetActive(false);
+            statisticsCanvas.enabled = false;
+        }
+        else
+        {
+            statisticsCanvas.gameObject.SetActive(true);
+            statisticsCanvas.enabled = true;
+        }
+
+    }
+    public void KYCButton_OnClick()
+    {
+        if (kycCanvas.gameObject.activeInHierarchy == true)
+        {
+            kycCanvas.gameObject.SetActive(false);
+            kycCanvas.enabled = false;
+
+        }
+        else
+        {
+            kycCanvas.gameObject.SetActive(true);
+            kycCanvas.enabled = true;
+        }
+
+    }
+    public void HomeButton_OnClick()
+    {
+        if (homeCanvas.gameObject.activeInHierarchy == true)
+        {
+            homeCanvas.gameObject.SetActive(false);
+            homeCanvas.enabled = false;
+
+        }
+        else
+        {
+            homeCanvas.gameObject.SetActive(true);
+            homeCanvas.enabled = true;
+        }
+
+    }
+    public void SideButton_OnClick()
+    {
+        if (sideCanvas.gameObject.activeInHierarchy == true)
+        {
+            sideCanvas.gameObject.SetActive(false);
+            sideCanvas.enabled = false;
+
+        }
+        else
+        {
+            sideCanvas.gameObject.SetActive(true);
+            sideCanvas.enabled = true;
+        }
+
+    }
+
+    public void SideBarProfile_OnClick()
+    {
+        if (basicCanvas.gameObject.activeInHierarchy == true)
+        {
+            basicCanvas.gameObject.SetActive(false);
+            basicCanvas.enabled = false;
+
+        }
+        else
+        {
+            basicCanvas.gameObject.SetActive(true);
+            basicCanvas.enabled = true;
+        }
+
+    }
+
+    public void SideBarTableList_OnClick()
+    {
+        if (tableListCanvas.gameObject.activeInHierarchy == true)
+        {
+            tableListCanvas.gameObject.SetActive(false);
+            tableListCanvas.enabled = false;
+
+        }
+        else
+        {
+            tableListCanvas.gameObject.SetActive(true);
+            tableListCanvas.enabled = true;
+        }
+
+    }
+
+    public void PopUp_OnClick()
+    {
+        if (popUpCanvas.gameObject.activeInHierarchy == true)
+        {
+            popUpCanvas.gameObject.SetActive(false);
+            popUpCanvas.enabled = false;
+
+        }
+        else
+        {
+            popUpCanvas.gameObject.SetActive(true);
+            tableListCanvas.enabled = true;
+        }
+
+    }
+
+
+
+}
